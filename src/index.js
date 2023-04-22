9eaf8ca72791491d9b8b550e95499d286e954bfa
Yaroslav Korin <lexuslx570vstoyota@gmail.com>
2023-04-22 19:36:21 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+import { fetchCountries } from './js/fetchCountries';
+
 
 const DEBOUNCE_DELAY = 300;
+const input = document.querySelector("#search-box");
+const list = document.querySelector("country-list");
+const info = document.querySelector("country-info");
+
+let searchCountryName = '';
+
+
