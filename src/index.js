1d961d8620ec6742d52bf6ca28389bfa25ff4c22
Yaroslav Korin <lexuslx570vstoyota@gmail.com>
2023-04-24 16:52:55 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,6 +9,52 @@ const input = document.querySelector("#search-box");
 const list = document.querySelector("country-list");
 const info = document.querySelector("country-info");
 
-let searchCountryName = '';
+// let searchCountryName = '';
+
+input.addEventListener('input', debounce(onCountryInput, DEBOUNCE_DELAY));
 
 
+function onCountryInput() {
+    const name = input.value.trim()
+
+    if (name === '') {
+     return (list.innerHTML = ''), (info.innerHTML = '')   
+    }
+
+    fetchCountries(name)
+        .then(countries => {
+            list.innerHTML = ''
+            info.innerHTML = ''
+
+            if (countries.length === 1) {
+                list.insertAdjacentHTML('beforeend', renderList(countries))
+                info.insertAdjacentHTML('beforeend', renderInfo(countries))
+            } else if (countries.lenght >= 10) {
+                alertTooManyMatches()
+            } else {
+                list.insertAdjacentHTML('beforeend', renderList(countries))
+            }
+        })
+    .catch(alertWrongName)
+}
+
+function renderInfo(countries) {
+    const markup = countries
+        .map(({ capital, population, languages }) => {
+        
+            return `<ul class="country-info__list">
+            <li class="country-info__item"><p><b>Capital: </b>${capital}</p></li>
+            <li class="country-info__item"><p><b>Population: </b>${population}</p></li>
+            <li class="country-info__item"><p><b>Languages: </b>${Object.values(languages).join(', ')}</p></li>
+        </ul>`
+        }).join('')
+    return markup
+}
+
+function alertWrongName() {
+    Notiflix.Notify.failure('Oops, there is no country with that name')
+}
+
+function alertTooManyMatches() {
+    Notiflix.Notify.failure('Too many matches found. Please enter a more specific name.')
+}
